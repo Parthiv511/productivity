fccdec873040eefe11ab7283ed596535372423bd
Parthiv511 <parthivk2007@gmail.com>
2026-06-13 20:22:28 +0530

    Time: 47 ms (22.73%), Space: 46.3 MB (30.75%) - LeetHub

---
+++ 0866-prime-palindrome/0866-prime-palindrome.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    public int primePalindrome(int n) {
+
+        if (8 <= n && n <= 11) {
+            return 11;
+        }
+
+        for (int i = 1; i < 100000; i++) {
+
+            String s = Integer.toString(i);
+            StringBuilder sb = new StringBuilder(s);
+
+            sb.deleteCharAt(sb.length() - 1);
+            sb.reverse();
+
+            int palindrome = Integer.parseInt(s + sb);
+
+            if (palindrome >= n && isPrime(palindrome)) {
+                return palindrome;
+            }
+        }
+
+        return -1;
+    }
+
+    private boolean isPrime(int num) {
+
+        if (num < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
